7309dd81f9bbea9e8847ee5135199451995bea3e
v6066 <51444058+v6066@users.noreply.github.com>
2021-01-29 11:41:43 +0800

    rand number

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace 双色球
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.rtx_redBall = new System.Windows.Forms.RichTextBox();
+            this.rtx_blueBall = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtx_gLuckBall = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "模拟选球";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 64);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "还原球池";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // rtx_redBall
+            // 
+            this.rtx_redBall.Location = new System.Drawing.Point(106, 25);
+            this.rtx_redBall.Name = "rtx_redBall";
+            this.rtx_redBall.Size = new System.Drawing.Size(348, 96);
+            this.rtx_redBall.TabIndex = 1;
+            this.rtx_redBall.Text = "";
+            // 
+            // rtx_blueBall
+            // 
+            this.rtx_blueBall.Location = new System.Drawing.Point(106, 146);
+            this.rtx_blueBall.Name = "rtx_blueBall";
+            this.rtx_blueBall.Size = new System.Drawing.Size(348, 96);
+            this.rtx_blueBall.TabIndex = 1;
+            this.rtx_blueBall.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "红 球 池";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "蓝 球 池";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "模拟中奖号";
+            // 
+            // rtx_gLuckBall
+            // 
+            this.rtx_gLuckBall.Location = new System.Drawing.Point(106, 365);
+            this.rtx_gLuckBall.Name = "rtx_gLuckBall";
+            this.rtx_gLuckBall.Size = new System.Drawing.Size(348, 73);
+            this.rtx_gLuckBall.TabIndex = 3;
+            this.rtx_gLuckBall.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtx_gLuckBall);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtx_blueBall);
+            this.Controls.Add(this.rtx_redBall);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox rtx_redBall;
+        private System.Windows.Forms.RichTextBox rtx_blueBall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtx_gLuckBall;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 双色球
+{
+    public partial class Form1 : Form
+    {//定义全局变量
+        Random myrd = new Random();
+        ArrayList redBalls = new ArrayList();
+        ArrayList blueBalls = new ArrayList();
+        int[] gLuckBalls = new int[7];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //随机产生红球
+            for (int i=0;i<=5 ;i++ )
+            {
+                int rdIndex = myrd.Next(0, 33-i);
+
+                gLuckBalls[i] = Convert.ToInt32(redBalls[rdIndex]);
+                redBalls.RemoveAt(rdIndex);
+
+                this.rtx_redBall.Clear();
+                foreach (object redB in redBalls)
+                {
+                    this.rtx_redBall.Text += redB + " ";
+                    }
+                         }
+            //随机产生蓝球
+                int blIndex = myrd.Next(0, 16);
+                gLuckBalls[6] = Convert.ToInt32(blueBalls[blIndex]);
+                blueBalls.RemoveAt(blIndex);
+
+                this.rtx_blueBall.Clear();
+                foreach (object blueB in blueBalls)
+                {
+                    this.rtx_blueBall.Text += blueB + " ";
+                }
+                //显示中奖号
+                foreach(int glb in gLuckBalls)
+            {
+                this.rtx_gLuckBall.Text += glb + " ";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //初始化动态数组蓝球池和红球池数组（红：1-33，蓝：1-16）
+            for(int i=1;i<=33 ;i++ )
+            {
+                redBalls.Add(i);
+            }
+            for (int j= 1; j <= 16; j++)
+            {
+                blueBalls.Add(j);
+            }
+            //显示红球池和蓝球池到窗体所对应的多行文本框中
+            foreach(object redB in redBalls)
+            {
+                this.rtx_redBall.Text += redB + " ";
+            }
+            foreach (object blueB in blueBalls)
+            {
+                this.rtx_blueBall.Text += blueB + " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.rtx_redBall.Text = "";
+            this.rtx_blueBall.Text = "";
+            this.rtx_gLuckBall.Text = "";
+            blueBalls.Clear();
+            redBalls.Clear();
+            Form1_Load(null, null);
+        }
+    }
+}
